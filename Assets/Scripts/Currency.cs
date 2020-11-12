79028a72fb542ec4d06872a9bfc412770bc3315a
Cristel Capetillo <cristel.capetillo@outlook.com>
2020-11-12 13:41:39 +0100

    Adds the quit button script

--- Assets/Scripts/Currency.cs
+++ Assets/Scripts/Currency.cs
@@ -5,6 +5,14 @@ public class Currency : MonoBehaviour {
 
     public int faithAmount;
     public Text faithAmountText;
+    
+    void Start() {
+        faithAmount = PlayerPrefs.GetInt("Cupcakes", 0);
+    }
+
+    void OnDestroy() {
+        PlayerPrefs.SetInt("Cupcakes", faithAmount);
+    }
 
     void Update() {
         faithAmountText.text = faithAmount.ToString("You have 0 Faith");
---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+    
+    public int faithAmount;
+    void Start() {
+        faithAmount = PlayerPrefs.GetInt("Cupcakes", 0);
+    }
+
+    void OnDestroy() {
+        PlayerPrefs.SetInt("Cupcakes", faithAmount);
+    }
+}
