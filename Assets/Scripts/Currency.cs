02e82d8d62046b3bc77b4ab56f4b68f5f5f93e48
Victorpetts <vike.pet@hotmail.com>
2020-11-12 14:32:55 +0100

    Makes faith gain configurable and adds code for monk

--- Assets/Scripts/Currency.cs
+++ Assets/Scripts/Currency.cs
@@ -2,24 +2,47 @@
 using UnityEngine.UI;
 
 public class Currency : MonoBehaviour {
-
-    public int faithAmount;
+    
     public Text faithAmountText;
+    public Text monkLvlText;
+    
+    public int faithPerClick = 5;
+
+    private int _faithAmount;
+    private int _monkLvl;
     
     void Start() {
-        faithAmount = PlayerPrefs.GetInt("Cupcakes", 0);
+        _faithAmount = PlayerPrefs.GetInt("Faith", 0);
+        _monkLvl = PlayerPrefs.GetInt("MonkLvl", 0);
     }
 
     void OnDestroy() {
-        PlayerPrefs.SetInt("Cupcakes", faithAmount);
+        PlayerPrefs.SetInt("Faith", _faithAmount);
+        PlayerPrefs.SetInt("MonkLvl", _monkLvl);
     }
 
-    void Update() {
-        faithAmountText.text = faithAmount.ToString("You have 0 Faith");
+    // void Update() {
+    //    faithAmountText.text = _faithAmount.ToString("You have 0 Faith");
+    // }
+    
+    public int CupcakeAmount {
+        get => this._faithAmount;
+        set {
+            _faithAmount = value;
+            faithAmountText.text = value.ToString("You have 0 faith");
+        } 
+    }
+    
+    public int MonkLvl {
+        get => this._monkLvl;
+        set {
+            _monkLvl = value;
+            monkLvlText.text = value.ToString("Monk level: 0");
+        } 
     }
     
     public void GainFaith() {
-        faithAmount += 5;
+        _faithAmount += faithPerClick;
     }
 }
 
