9769df4eb031f6961d90bdfafef3a3dd1b925d16
Cristel Capetillo <cristel.capetillo@outlook.com>
2020-11-12 11:47:59 +0100

    Adds a working GainFaith button script

---
+++ Assets/Scripts/Currency.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour {
+
+    public int faithAmount;
+    public Text faithAmountText;
+
+    void Update() {
+        faithAmountText.text = faithAmount.ToString("You have 0 Faith");
+    }
+    
+    public void GainFaith() {
+        faithAmount += 5;
+    }
+}
+
+
+
+
+
+
+
+
