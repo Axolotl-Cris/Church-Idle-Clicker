1bc111c183fdeefa8e208171aa58d50e6aee83ce
Cristel Capetillo <cristel.capetillo@outlook.com>
2020-11-12 15:21:37 +0100

    Adds a fix for the Get & Set

--- Assets/Scripts/Currency.cs
+++ Assets/Scripts/Currency.cs
@@ -4,26 +4,19 @@ using UnityEngine.UI;
 public class Currency : MonoBehaviour {
     
     public Text faithAmountText;
-    public Text monkLvlText;
     
     public int faithPerClick = 5;
 
     private int _faithAmount;
-    private int _monkLvl;
     
     void Start() {
-        _faithAmount = PlayerPrefs.GetInt("Faith", 0);
-        _monkLvl = PlayerPrefs.GetInt("MonkLvl", 0);
+        FaithAmount = PlayerPrefs.GetInt("Faith", 0);
     }
 
     void OnDestroy() {
-        PlayerPrefs.SetInt("Faith", _faithAmount);
-        PlayerPrefs.SetInt("MonkLvl", _monkLvl);
+        PlayerPrefs.SetInt("Faith", FaithAmount);
     }
-
-    void Update() { 
-        faithAmountText.text = _faithAmount.ToString("You have 0 Faith");
-    }
+    
     
     public int FaithAmount {
         get => this._faithAmount;
@@ -33,16 +26,9 @@ public class Currency : MonoBehaviour {
         } 
     }
     
-    public int MonkLvl {
-        get => this._monkLvl;
-        set {
-            _monkLvl = value;
-            monkLvlText.text = value.ToString("Monk level: 0");
-        } 
-    }
     
     public void GainFaith() {
-        _faithAmount += faithPerClick;
+        FaithAmount += faithPerClick;
     }
 }
 
