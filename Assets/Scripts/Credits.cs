8d2c9db28a1e8645c655e20fd299c23da79fe89d
MissSassaJ <sandrajohansson_91@hotmail.se>
2020-11-20 10:14:01 +0100

    It made changes to production/start menu

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+
+    public GameObject logoText;
+
+    public void Start()
+    {
+        logoText.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        logoText.SetActive(true);
+        Debug.Log("Mouse is over GameObject.");
+    }
+
+    public void OnMouseExit()
+    {
+        logoText.SetActive(false);
+        Debug.Log("Mouse is no longer on GameObject.");
+    }
+
+}
